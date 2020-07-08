46b74f30f55a4fe272d9aaa3cd366a1647131887
payal <payal@aum.bz>
2020-07-08 09:53:28 +0530

    translatable

--- src/DataFixtures/CountryFixtures.php
+++ src/DataFixtures/CountryFixtures.php
@@ -13,17 +13,29 @@ class CountryFixtures extends Fixture
         // $tag1->setName("india");
         // $manager->persist($tag1);
 
-        $tag1 = $manager->find('App\Entity\Country',24);
-        $tag1->setName('india in hi');
-        $tag1->setTranslatableLocale('hi'); // change locale
-        $manager->persist($tag1);
+        // $tag1 = $manager->find('App\Entity\Country',24);
+        // $tag1->setName('india in hi');
+        // $tag1->setTranslatableLocale('hi'); // change locale
+        // $manager->persist($tag1);
 
         // $tag2 = new Country();
         // $tag2->setName("US");
-        
+        $repository = $manager->getRepository('Gedmo\\Translatable\\Entity\\Translation');
+// it works for ODM also
+$country = new Country;
+$country->setName('My article en');
+
+
+$repository->translate($country, 'name', 'hi', 'my article hi')
+ 
+;
+
+$manager->persist($country);
+
+
        
         //$manager->persist($tag2);
-        $this->addReference('country-1', $tag1);
+        $this->addReference('country-1', $country);
         //$this->addReference('country-2', $tag2);
 
         $manager->flush();

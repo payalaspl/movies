2e7450a411011c3015b54d74a28b0b33209ca418
payal <payal@aum.bz>
2020-07-08 10:14:30 +0530

    add country

--- src/EventSubscriber/DoctrineExtensionListener.php
+++ src/EventSubscriber/DoctrineExtensionListener.php
@@ -4,10 +4,11 @@
 
 namespace App\EventSubscriber;
 
+
+
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\HttpKernel\Kernel;
 
 class DoctrineExtensionListener implements ContainerAwareInterface
 {
@@ -27,38 +28,12 @@ class DoctrineExtensionListener implements ContainerAwareInterface
         $translatable->setTranslatableLocale($event->getRequest()->getLocale());
     }
 
-    public function onConsoleCommand()
-    {
-        $this->container->get('gedmo.listener.translatable')
-            ->setTranslatableLocale($this->container->get('translator')->getLocale());
-    }
-
     public function onKernelRequest(GetResponseEvent $event)
     {
-        if (Kernel::MAJOR_VERSION == 2 && Kernel::MINOR_VERSION < 6) {
-            $securityContext = $this->container->get('security.context', ContainerInterface::NULL_ON_INVALID_REFERENCE);
-            if (null !== $securityContext && null !== $securityContext->getToken() && $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-                # for loggable behavior
-                $loggable = $this->container->get('gedmo.listener.loggable');
-                $loggable->setUsername($securityContext->getToken()->getUsername());
-
-                # for blameable behavior
-                $blameable = $this->container->get('gedmo.listener.blameable');
-                $blameable->setUserValue($securityContext->getToken()->getUser());
-            }
-        }
-        else {
-            $tokenStorage = $this->container->get('security.token_storage')->getToken();
-            $authorizationChecker = $this->container->get('security.authorization_checker');
-            if (null !== $tokenStorage && $authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-                # for loggable behavior
-                $loggable = $this->container->get('gedmo.listener.loggable');
-                $loggable->setUsername($tokenStorage->getUser());
-
-                # for blameable behavior
-                $blameable = $this->container->get('gedmo.listener.blameable');
-                $blameable->setUserValue($tokenStorage->getUser());
-            }
+        $securityContext = $this->container->get('security.context', ContainerInterface::NULL_ON_INVALID_REFERENCE);
+        if (null !== $securityContext && null !== $securityContext->getToken() && $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $loggable = $this->container->get('gedmo.listener.loggable');
+            $loggable->setUsername($securityContext->getToken()->getUsername());
         }
     }
 }
